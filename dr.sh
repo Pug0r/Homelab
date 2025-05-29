fd3350c9d13d34cc055bbe80cb77264370f6b3fb
Aleksander <aleksanderpugowski@gmail.com>
2025-05-29 21:51:57 +0200

    Change script args order

--- dr.sh
+++ dr.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [[ "$1" = "up" ]]; then
-    docker compose --env-file .env -f docker/$2/docker-compose.yml up -d
-elif [[ "$1" = "down" ]]; then
-    docker compose -f docker/$2/docker-compose.yml down
-elif [[ "$1" = "restart" ]]; then
-    docker compose --env-file .env -f docker/$2/docker-compose.yml restart 
+if [[ "$2" = "up" ]]; then
+    docker compose --env-file .env -f docker/$1/docker-compose.yml up -d
+elif [[ "$2" = "down" ]]; then
+    docker compose -f docker/$1/docker-compose.yml down
+elif [[ "$2" = "restart" ]]; then
+    docker compose --env-file .env -f docker/$1/docker-compose.yml restart 
 else
-    echo "Usage: ./dr.sh [up/down/restart] [name]"
+    echo "Usage: ./dr.sh [name] [up/down/restart]"
 fi
 
